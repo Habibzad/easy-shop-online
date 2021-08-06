76711d5c1a1943517a1401ffed1bb222c91cc987
Ahmad Habibzad <masoud.habibzad@gmail.com>
2021-08-05 23:48:47 -0400

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { Card, Button } from "react-bootstrap";
+
+function Home() {
+  const [product] = useState({
+    id: 1,
+    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    price: 109.95,
+    description:
+      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    category: "men's clothing",
+    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  });
+  return (
+    <Card style={{ width: "20rem" }}>
+      <Card.Img variant="top" src={product.image} />
+      <Card.Body>
+        <Card.Title>{product.title}</Card.Title>
+        <Card.Text>{product.description}</Card.Text>
+        <Button variant="primary">Add to the card</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Home;
